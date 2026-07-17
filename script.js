5dd476f1623c326f303b366df00fa3f700b93dff
editord905-lgtm <editord905@gmail.com>
2026-07-17 15:28:54 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,60 @@
+window.onload = () => {
+
+const loading = document.getElementById("loading-screen");
+const experience = document.getElementById("experience");
+
+setTimeout(() => {
+    loading.classList.add("hidden");
+    experience.classList.remove("hidden");
+}, 3000);
+
+
+const intro = document.getElementById("intro");
+const travel = document.getElementById("travel");
+const regulus = document.getElementById("regulus");
+
+setTimeout(() => {
+    intro.style.display = "none";
+    travel.style.display = "block";
+}, 9000);
+
+
+setTimeout(() => {
+    travel.style.display = "none";
+    regulus.style.display = "block";
+}, 14000);
+
+
+// estrelas viajando
+
+const starfield = document.getElementById("starfield");
+
+for(let i = 0; i < 120; i++){
+
+    let star = document.createElement("span");
+
+    star.innerHTML = "✦";
+    star.style.position = "absolute";
+    star.style.left = Math.random()*100 + "%";
+    star.style.top = Math.random()*100 + "%";
+    star.style.fontSize = Math.random()*10 + 5 + "px";
+    star.style.opacity = Math.random();
+
+    starfield.appendChild(star);
+
+}
+
+
+// carta
+
+const button = document.getElementById("open-letter");
+const letter = document.getElementById("letter");
+
+button.onclick = () => {
+
+    regulus.style.display = "none";
+    letter.classList.remove("hidden");
+
+};
+
+};
